20f8d9262c2260733975d69eae7f71ac781e2356
t.io <g_t@gamil.com>
2023-10-24 08:47:00 +0800

    static add Rewrite

--- http/static.go
+++ http/static.go
@@ -3,22 +3,52 @@ package http
 import (
 	"net/http"
 	"path"
+	"regexp"
 	"strings"
 	"time"
 )
 
 var DefaultStatic = NewStatic()
 
-type staticEntry struct {
-	fs      http.FileSystem
-	dirPath string
+type (
+	StaticOps interface {
+		Rewrite(orig string, replace string)
+	}
+	staticEntry struct {
+		fs      http.FileSystem
+		dirPath string
+		rewrite map[string]string
+	}
+	StaticOption func(StaticOps)
+)
+
+func StaticRewrite(orig string, replace string) StaticOption {
+	return func(ops StaticOps) {
+		ops.Rewrite(orig, replace)
+	}
+}
+
+func (s *staticEntry) Rewrite(orig string, replace string) {
+	s.rewrite[orig] = replace
 }
 
 func (s *staticEntry) ServeHTTP(rw http.ResponseWriter, req *http.Request) bool {
 	if req.URL.Path == "/" {
 		req.URL.Path = "/index.html"
 	}
-	f, err := s.fs.Open(path.Join(s.dirPath, req.URL.Path))
+	//rewrite path
+	pathWriter := req.URL.Path
+	for orig, s3 := range s.rewrite {
+		compile, err := regexp.Compile(orig)
+		if err != nil {
+			panic(err)
+		}
+		if o1 := compile.FindString(pathWriter); o1 != "" {
+			pathWriter = strings.Replace(pathWriter, o1, s3, 1)
+		}
+	}
+
+	f, err := s.fs.Open(path.Join(s.dirPath, pathWriter))
 	if err != nil {
 		return false
 	}
@@ -55,10 +85,15 @@ func (s *Static) Http(rw http.ResponseWriter, req *http.Request) bool {
 // path is the mapping url
 // dirPath is the real path
 // fileSystem open[ join(path + dirPath) ]
-func (s *Static) HandleStatic(path, dirPath string, fileSystem http.FileSystem) {
-	s.m[path] = staticEntry{
+func (s *Static) HandleStatic(path, dirPath string, fileSystem http.FileSystem, sOps ...StaticOption) {
+	entry := staticEntry{
 		fs:      fileSystem,
 		dirPath: dirPath,
+		rewrite: make(map[string]string),
+	}
+	s.m[path] = entry
+	for _, op := range sOps {
+		op(&entry)
 	}
 }
 
