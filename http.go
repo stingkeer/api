53137ce7138c8e1db481b4bc0db4a460f9f128b1
safe <safe.ok@outlook.com>
2023-10-27 15:35:30 +0800

    Add error

--- http.go
+++ http.go
@@ -3,6 +3,8 @@
 package api
 
 import (
+	"crypto/tls"
+	"net"
 	"net/http"
 
 	"gitee.com/fast_api/api/def"
@@ -12,7 +14,7 @@ import (
 
 var server = NewServer(def.DefaultContext.Pool)
 
-func StartService(ops ...Optional) {
+func StartService(ops ...Optional) error {
 	apply(&defaultConf, ops...)
 	server := &http.Server{
 		Addr:    defaultConf.listen,
@@ -20,22 +22,46 @@ func StartService(ops ...Optional) {
 	}
 	if err := server.ListenAndServe(); err != nil {
 		log.Error(err)
+		return err
 	}
+	return nil
 }
 
-func StartTLSService(ops ...Optional) {
+func StartTLSService(ops ...Optional) error {
 	apply(&defaultConf, ops...)
 	t, err := loadTls(&defaultConf)
 	if err != nil {
 		log.Error(err)
-		return
+		return err
 	}
 	server := &http.Server{
 		Addr:      defaultConf.listen,
 		Handler:   server,
 		TLSConfig: t,
 	}
-	if err := server.ListenAndServe(); err != nil {
+	config := t.Clone()
+
+	// if !strSliceContains(config.NextProtos, "http/1.1") {
+	// 	config.NextProtos = append(config.NextProtos, "http/1.1")
+	// }
+
+	// if server.shuttingDown() {
+	// 	return http.ErrServerClosed
+	// }
+
+	addr := server.Addr
+	if addr == "" {
+		addr = ":https"
+	}
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	defer ln.Close()
+	tlsListener := tls.NewListener(ln, config)
+	if err := server.Serve(tlsListener); err != nil {
 		log.Error(err)
 	}
+	return nil
 }
--- http3.go
+++ http3.go
@@ -3,6 +3,8 @@
 package api
 
 import (
+	"errors"
+
 	"gitee.com/fast_api/api/def"
 	_ "gitee.com/fast_api/api/kit"
 	"gitee.com/fast_api/api/log"
@@ -11,18 +13,18 @@ import (
 
 var server = NewServer(def.DefaultContext.Pool)
 
-func StartService(ops ...Optional) {
-	panic("http3 only support tls")
+func StartService(ops ...Optional) error {
+	return errors.New("http3 only support tls")
 }
 
-func StartTLSService(ops ...Optional) {
+func StartTLSService(ops ...Optional) error {
 	apply(&defaultConf, ops...)
 	log.Infof("listen addr %s")
 
 	t, err := loadTls(&defaultConf)
 	if err != nil {
 		log.Error(err)
-		return
+		return err
 	}
 	server := &http3.Server{
 		Addr:      defaultConf.listen,
@@ -31,5 +33,7 @@ func StartTLSService(ops ...Optional) {
 	}
 	if err := server.ListenAndServe(); err != nil {
 		log.Error(err)
+		return err
 	}
+	return nil
 }
