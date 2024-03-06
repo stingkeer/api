624f8764af7c594b3945d5d4747b977024210475
t.io <g_t@gamil.com>
2024-03-06 19:21:45 +0800

    http: fix *big.int

--- call/types/big_type.go
+++ call/types/big_type.go
@@ -14,13 +14,20 @@ type BigType struct {
 }
 
 func (f BigType) Mapper(param *def.ParamWarp) reflect.Value {
-	if in, b := new(big.Int).SetString(param.PValue, 10); b {
-		return reflect.ValueOf(*in).Convert(param.PTyp)
+	if param.PTyp.Kind() == reflect.Struct {
+		if in, b := new(big.Int).SetString(param.PValue, 10); b {
+			return reflect.ValueOf(*in).Convert(param.PTyp)
+		}
+	}
+	if param.PTyp.Kind() == reflect.Ptr {
+		if in, b := new(big.Int).SetString(param.PValue, 10); b {
+			return reflect.ValueOf(in).Convert(param.PTyp)
+		}
 	}
 	panic(fmt.Sprintf("BigType error param %s", param.PValue))
 
 }
 
 func (f BigType) Register() []reflect.Type {
-	return []reflect.Type{reflect.TypeOf((*big.Int)(nil)).Elem()}
+	return []reflect.Type{reflect.TypeOf((*big.Int)(nil)).Elem(), reflect.TypeOf((*big.Int)(nil))}
 }
